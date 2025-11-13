9d9fee8078885aa71c4b1ba0590c3c88c6190dab
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-11-13 09:42:50 +0900

    Initial commit

---
+++ src/main/java/net/watchpeople/WatchPeopleApplication.java
@@ -0,0 +1,13 @@
+package net.watchpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchPeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchPeopleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/watchpeople/WatchPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.watchpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchPeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
