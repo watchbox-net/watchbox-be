aa4da24c71393bea4485842048a5f667ead69b93
chyun0029 <chyun0029@gmail.com>
2025-11-13 09:46:58 +0900

    .

--- src/main/java/net/watchpeople/WatchPeopleApplication.java
+++ src/main/java/net/watchpeople/WatchPeopleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WatchPeopleApplication {
+public class WatchPeopleApplication { //
 
     public static void main(String[] args) {
         SpringApplication.run(WatchPeopleApplication.class, args);
