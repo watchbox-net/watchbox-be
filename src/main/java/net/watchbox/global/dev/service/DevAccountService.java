3c6d44cb6b39de7a5db25e5fd8b0a9afd52e2f04
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2025-12-15 16:41:39 +0900

    feat: 개발용 계정 생성 서비스 로직 구현

---
+++ src/main/java/net/watchbox/global/dev/service/DevAccountService.java
@@ -0,0 +1,31 @@
+package net.watchbox.global.dev.service;
+
+import lombok.RequiredArgsConstructor;
+import net.watchbox.domain.account.entity.OauthAccount;
+import net.watchbox.domain.account.entity.OauthProvider;
+import net.watchbox.domain.account.repository.OauthAccountRepository;
+import net.watchbox.domain.member.entity.Member;
+import net.watchbox.domain.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DevAccountService {
+    private final OauthAccountRepository oauthAccountRepository;
+    private final MemberRepository memberRepository;
+
+    public Member createDevMember() {
+        OauthAccount oauthAccount = OauthAccount.builder()
+                .oauthProvider(OauthProvider.GOOGLE)
+                .oauthId("tester-oauth-id")
+                .email("tester0@gmail.com")
+                .name("tester0")
+                .build();
+        Member member = Member.builder()
+                .oauthAccount(oauthAccount)
+                .email(oauthAccount.getEmail())
+                .nickname(oauthAccount.getName())
+                .build();
+        return memberRepository.save(member);
+    }
+}
