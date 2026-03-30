b8ea0866a1f8b219bf715347823b9dc00324d5ad
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2026-03-30 12:15:43 +0900

    deploy: 운영서버 배포 workflow 작성

--- src/main/java/net/watchbox/global/config/SwaggerConfig.java
+++ src/main/java/net/watchbox/global/config/SwaggerConfig.java
@@ -6,11 +6,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
