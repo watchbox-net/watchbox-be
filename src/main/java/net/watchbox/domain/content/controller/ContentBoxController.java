b4f9d89c9a476b739e316d37d759e772da63a055
chyun5197 <chyun5197@gmail.com>
2026-04-17 20:28:44 +0900

    chore

--- src/main/java/net/watchbox/domain/content/controller/ContentBoxController.java
+++ src/main/java/net/watchbox/domain/content/controller/ContentBoxController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class ContentBoxController {
     private final ContentBoxFacade contentBoxFacade;
 
-    @Operation(summary = "사용자 모든 박스에 컨텐츠 포함 여부 조회")
+    @Operation(summary = "컨텐츠가 사용자 박스들에 저장되어있는지 유무 조회")
     @GetMapping
     public ResponseEntity<ApiResponse<ContentBoxSheetResponse>> getContentBoxSheet(
             @AuthenticationPrincipal Member member,
@@ -33,7 +33,7 @@ public class ContentBoxController {
         ));
     }
 
-    @Operation(summary = "컨텐츠 일괄 추가/삭제")
+    @Operation(summary = "컨텐츠를 사용자 박스들에 일괄 추가/삭제")
     @PostMapping
     public ResponseEntity<ApiResponse<ContentBoxUpdateResponse>> updateContentBoxes(
             @AuthenticationPrincipal Member member,
