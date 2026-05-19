af42ee2672d9a3a1891e2cf8966a809bdb0094e1
chyun5197 <chyun5197@gmail.com>
2026-05-19 16:28:54 +0900

    feat: Redis lettuce 설정 상세

--- src/main/java/net/watchbox/global/config/RedisConfig.java
+++ src/main/java/net/watchbox/global/config/RedisConfig.java
@@ -10,10 +10,10 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
-    @Value("${redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Bean // Lettuce - 토큰 저장, 캐싱에 사용
