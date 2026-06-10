e674f43f419bde7e8b6892186cf2b58e62cf5cd8
chyun5197 <chyun5197@gmail.com>
2026-06-10 15:50:54 +0900

    refactor: 컨텐츠 박스에 추가 시 알림 위치를 BoxContent -> ContentBox 컨트롤러로 옮김. 페이로드명도 ContentBoxAddedXXX로 수정

--- src/main/java/net/watchbox/domain/notification/dto/payload/NotificationPayload.java
+++ src/main/java/net/watchbox/domain/notification/dto/payload/NotificationPayload.java
@@ -24,10 +24,10 @@ import net.watchbox.domain.notification.entity.NotificationType;
 @JsonSubTypes({
         @JsonSubTypes.Type(value = BoxInvitationPayload.class, name = "BOX_INVITATION_RECEIVED"),
         @JsonSubTypes.Type(value = BoxInvitationRespondedPayload.class, name = "BOX_INVITATION_RESPONDED"),
-        @JsonSubTypes.Type(value = BoxContentAddedPayload.class, name = "BOX_CONTENT_ADDED")
+        @JsonSubTypes.Type(value = ContentBoxAddedPayload.class, name = "BOX_CONTENT_ADDED")
 })
 public sealed interface NotificationPayload
-        permits BoxInvitationPayload, BoxInvitationRespondedPayload, BoxContentAddedPayload {
+        permits BoxInvitationPayload, BoxInvitationRespondedPayload, ContentBoxAddedPayload {
 
     /** 페이로드의 알림 타입 (NotificationType enum 과 1:1). */
     NotificationType type();
